e94e58e897dbed6c6c04a194855111ac68bebdc2
LabialDaryl <daryllabial1@gmail.com>
2026-05-23 13:31:51 +0800

    Remove tracked .venv and prepare repo

--- app.py
+++ app.py
@@ -1232,6 +1232,10 @@ def api_profile(current_user):
 
 if __name__ == '__main__':
 
+    port = int(os.environ.get('PORT', 5000))
+
     app.run(
-        debug=True
+        host='0.0.0.0',
+        port=port,
+        debug=False
     )
